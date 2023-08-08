3c7beafeffb08b0ababca5e91a2e436195c785e5
invictalex <alexchristodoulou@ymail.com>
2023-08-08 11:31:21 +0100

    Add basic style to Recipe Card

--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect} from 'react'
 import downArrow from "./assets/down-arrow.svg"
+import placeholder from "./assets/placeholder.jpg"
 
 
 export default function Recipes(props) {
@@ -10,15 +11,41 @@ export default function Recipes(props) {
     let recipe = data.recipe
     const {label, image, ingredients, cuisineType} = recipe
 
+    console.log(placeholder)
+
     return (
-    <div key={index}><h6>{label} is recipe number {index + 1}</h6></div>)
+    
+    <div className="recipe-card-container" key={index}>
+
+      <div className="recipe-card">
+        <div className="recipe-card-image" style={{backgroundImage: `url(${placeholder})`}}>
+
+        </div>
+        <div className="recipe-card-info">
+          <div className="recipe-card-tags">
+            <div className="recipe-card-course">main</div>
+            <div className="recipe-card-cuisine">french</div>
+          </div>
+          
+          <h2 className="recipe-card-title">This is the title</h2>
+          <div className="recipe-card-ingredients-counter">
+            <p>You have 5/7 ingredients</p>
+          </div>  
+          <div className="recipe-card-expand-button">Cook</div>
+        </div>
+      </div>
+      
+      
+      
+    </div>)
 })
 
     return (
         <section>
           <button className="get-recipes-btn" onClick={props.onSubmit}>Get Recipes<img src={downArrow} style={{width: "25px", display: "block", margin: "auto"}} /></button>
-        
+        <div className="recipe-card-section">
           {recipeCards}
+        </div>
         
         </section>
           
